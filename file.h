7725767d46904e533a2ec57f78b2c3c9b361998f
grady.white <grady.white@maine.edu>
2022-01-03 13:04:34 -0500

    Updated outline, added more skeletons

---
+++ file.h
@@ -0,0 +1,8 @@
+//  File Control
+//  Dynamic Strain Sensor
+//  Grady White
+//  1/3/22
+#ifndef _FILE_H_
+#define _FILE_H_
+
+#endif //_FILE_H_
---
+++ main.c
@@ -0,0 +1,7 @@
+//  Start of program
+//  Dynamic Strain Sensor
+//  Grady White
+//  1/3/22
+
+#include "serial.h"
+#include "runtime.h"
